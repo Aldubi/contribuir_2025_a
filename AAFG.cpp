95b92045e5fa9cdae67c6bf87f118144bc3ba423
alex <aledrive16@hotmail.com>
2025-03-16 09:58:36 -0600

    Agrego la clase de Alex Alberto Flores Guzman

---
+++ AAFG.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class AAFG{
+	private :
+		string cui,nombre,apellidos;
+		
+		void crear();
+		void leer ();
+		void actualizar ();
+		void borrar ();
+};
